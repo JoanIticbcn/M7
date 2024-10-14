16a2b6dbb939c0de64b8f913d393502b9b332561
Joan <joan@gmail.com>
2024-10-14 18:56:10 +0200

    Dia 0

---
+++ Activitat5/ExerciciC.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+IDs = [3,13,34,56,70,85,110,120,210,400]
+DataFrame = pd.read_csv("mobiles.csv",usecols=["id","clock_speed","px_height","px_width","battery_power"])
+
+def getClockSpeed():
+    dataClockSpeed = DataFrame[DataFrame['id'].isin(IDs)][['id', 'clock_speed']]
+    return dataClockSpeed
+
+def getMegapixels():
+    dataMegapixels = DataFrame[DataFrame['id'].isin(IDs)][['id','px_height','px_width']]
+    return dataMegapixels
+
+def getBatteryPower():
+    dataBatteryPower = DataFrame[DataFrame['id'].isin(IDs)][['id','battery_power']]
+    return dataBatteryPower
---
+++ Activitat5/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from ExerciciC import *
+
+clockSpeed = getClockSpeed()
+megaPixels = getMegapixels()
+batteryPower = getBatteryPower()
+
+plt.plot.bar()
+plt.show()
