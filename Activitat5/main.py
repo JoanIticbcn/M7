10f9a0e07b83aab6317d4dbb298abdeb397e18bf
Joan <joan@gmail.com>
2024-10-16 20:38:29 +0200

    Versio definitiva

--- Activitat5/main.py
+++ Activitat5/main.py
@@ -10,19 +10,19 @@ batteryPower = getBatteryPower()
 plt.figure(figsize=(15, 10))
 
 #Primer grafic
-plt.subplot(2, 2, 1)  # 1 row, 2 columns, 1st position
+plt.subplot(2, 2, 1) 
 plt.bar(clockSpeed['id'], clockSpeed['clock_speed'], color='skyblue')
 plt.title('Clockspeed per dispositiu')
 plt.xlabel('ID')
 plt.ylabel('Clockspeed en Gigaherz')
 #Segon grafic
-plt.subplot(2, 2, 2)  # 1 row, 2 columns, 1st position
+plt.subplot(2, 2, 2)
 plt.bar(megaPixels['id'], megaPixels['calculMegapixels'],color='yellow')
 plt.title('Megapixels per dispositiu')
 plt.xlabel('ID')
 plt.ylabel('Megapixels en millons')
 #Tercer grafic
-plt.subplot(2, 2, 3)  # 1 row, 2 columns, 1st position
+plt.subplot(2, 2, 3)
 plt.bar(batteryPower['id'], batteryPower['battery_power'],color='green')
 plt.title('Batterypower per dispositiu')
 plt.xlabel('ID')
